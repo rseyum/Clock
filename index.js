89c270636c1e88bca441378cbbd6b524bc110de0
rseyum <rseyum@sea.yearup.org>
2022-07-05 20:04:08 -0700

    code cleanup after test

--- index.js
+++ index.js
@@ -21,27 +21,24 @@ function showTime() {
   document.getElementById("par2").innerHTML =
     "&copy; " + "By Rahel Seyum" + " " + currentYear;
 
-  if (h == 5 || h == 6 || h == 7 || h == 8 || h == 9 || h == 10 || h == 11) {
+  if (h >= 5 && h < 12 && h) {
     h1.innerHTML = "Good Morning";
     par.innerHTML = "It's a morning time.";
     image.src = "img/morning.png";
     document.body.style.backgroundColor = "orange";
-  }
-  if (h == 12 || h == 13 || h == 14 || h == 15 || h == 16 || h == 17) {
+  } else if (h >= 12 && h < 18) {
     h1.innerHTML = "Good Afternoon";
     par.innerHTML = "It's a afternoon time.";
     image.src = "img/afternoon.png";
     document.body.style.backgroundColor = "blue";
     document.body.style.color = "white";
-  }
-  if (h == 18 || h == 19 || h == 20 || h == 21 || h == 22) {
+  } else if (h >= 18 && h <= 23) {
     h1.innerHTML = "Good evening";
     par.innerHTML = "It's a evening time.";
     image.src = "img/evening.png";
     document.body.style.backgroundColor = "green";
     document.body.style.color = "white";
-  }
-  if (h == 23 || h == 0 || h == 1 || h == 2 || h == 3 || h == 4) {
+  } else {
     h1.innerHTML = "Good night";
     par.innerHTML = "It's a night time.";
     image.src = "img/night.png";
